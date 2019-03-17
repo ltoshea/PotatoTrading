3a0d34c8ef2e09336201145c51634adf51cfbca2
ltoshea <ltoshea+github@gmail.com>
2019-03-17 14:48:33 +0000

    Removed original flask skeleton;

--- webapp/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, DecimalField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class InfoForm(FlaskForm):
-    type_of_potato = SelectField('Potato Type', validators=[DataRequired()])
-    fee = DecimalField('Fee', validators=[DataRequired()])
-    broker = SelectField('Broker')
-    submit = SubmitField('Save')
